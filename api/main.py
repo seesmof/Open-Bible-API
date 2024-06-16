b04e02b942794b0a0f77afe4811c088f1f3be6f6
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-06-16 21:07:57 +0300

    feat: Hallelujah thank You Jesus Christ our Holy Lord GOD Almighty

--- api/main.py
+++ api/main.py
@@ -5,6 +5,7 @@ from models import db
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = db.db
 db.init_app(app)
 with app.app_context():
     db.create_all()
