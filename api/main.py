d2a22918115025c5778ba36af2472c2b39ad8803
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-06-16 20:46:24 +0300

    feat: Hallelujah thank You Jesus Christ our Holy Lord GOD Almighty

--- api/main.py
+++ api/main.py
@@ -3,16 +3,27 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
-@app.route("/verse", methods=["GET"])
-def get_verse():
+@app.route("/<reference>", methods=["GET"])
+def get_verse(reference):
+    """
+    reference: str - main parameter for the verse reference
+
+    random: bool - returns random verse
+    """
     verse: dict = {
         "verse": "For GOD so loved the world that He gave His Only Son that whoever believes in Him should not perish but have eternal life.",
         "reference": "John 3:16",
     }
+    verse["reference"] = reference
 
     return jsonify(verse)
 
 
-@app.route("/", methods=["GET", "POST", "PUT", "DELETE"])
-def index():
-    return "go to /verse to get the verse"
+"""
+Bible database schema for getting random Bible verses 
+
+text: str 
+reference: str 
+lang: str = "en" | "uk"
+version: str = "NASB" | "UBIO"
+"""
