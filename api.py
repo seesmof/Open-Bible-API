e1d79d93e4cde90a421689362aef65f7053d0a92
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-06-16 21:42:58 +0300

    feat: Hallelujah thank You Jesus Christ our Holy Lord GOD Almighty

--- api.py
+++ api.py
@@ -2,7 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+verses: list[dict[str, str]] = [
+    {
+        "text": "For the wages of sin is death, but the gracious gift of God is eternal life in Christ Jesus our Lord",
+        "reference": "Romans 6:23",
+    },
+    {
+        "text": "For God so loved the world, that He gave His only Son, so that everyone who believes in Him will not perish, but have eternal life",
+        "reference": "John 3:16",
+    },
+]
 
-@app.get("/")
-async def root():
-    return {"message": "Hallelujah thank YOU Jesus Christ our Holy Lord GOD Almighty"}
+
+@app.get("/{reference}")
+def get_verse(reference: str):
+    for verse in verses:
+        if verse["reference"] == reference:
+            return verse
