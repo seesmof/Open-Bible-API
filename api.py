885ee4b4dd45dd6af4da2d5d9e853bc912424d79
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-06-20 13:51:32 +0300

    feat: more Verses ✝️ praise Jesus Christ our HOly Lord GOD ALmighty

--- api.py
+++ api.py
@@ -22,6 +22,42 @@ verses: list[dict[str, str]] = [
         "text": "For God so loved the world, that He gave His only Son, so that everyone who believes in Him will not perish, but have eternal life",
         "reference": "John 3:16",
     },
+    {
+        "text": "for all have sinned and fall short of the glory of God",
+        "reference": "Romans 3:23",
+    },
+    {
+        "text": "But God demonstrates His own love toward us, in that while we were still sinners, Christ died for us.",
+        "reference": "Romans 5:8",
+    },
+    {
+        "text": "that if you confess with your mouth Jesus as Lord, and believe in your heart that God raised Him from the dead, you will be saved; for with the heart a person believes, resulting in righteousness, and with the mouth he confesses, resulting in salvation.",
+        "reference": "Romans 10:9-10",
+    },
+    {
+        "text": "for “Everyone who calls on the name of the Lord will be saved.”",
+        "reference": "Romans 10:13",
+    },
+    {
+        "text": "Therefore, having been justified by faith, we have peace with God through our Lord Jesus Christ",
+        "reference": "Romans 5:1",
+    },
+    {
+        "text": "For whatever was written in earlier times was written for our instruction, so that through perseverance and the encouragement of the Scriptures we might have hope.",
+        "reference": "Romans 15:4",
+    },
+    {
+        "text": "Therefore there is now no condemnation at all for those who are in Christ Jesus.",
+        "reference": "Romans 8:1",
+    },
+    {
+        "text": "Jesus said to him, “I am the way, and the truth, and the life; no one comes to the Father except through Me.",
+        "reference": "John 14:6",
+    },
+    {
+        "text": "Jesus responded and said to him, “Truly, truly, I say to you, unless someone is born again he cannot see the kingdom of God.”",
+        "reference": "John 3:3",
+    },
 ]
 
 
