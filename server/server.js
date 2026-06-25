0a031c4b100f5db4d32f138da82c76e50bef963b
Oleg Onyshchenko <seesmwork@gmail.com>
2026-06-25 21:37:06 +0300

    refactor: remove FastAPI, add Express

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const Bible = require("./bible.json")
+
+const app = express();
+const port = 3000;
+
+app.get("/:book/:chapter", (req, res) => {
+    const book = req.params.book;
+    let chapter = req.params.chapter;
+
+    if (!chapter || chapter < 1 || chapter > 150) chapter = 1;
+
+    const BibleBooks = Object.keys(Bible);
+    if (!BibleBooks.includes(book)) return res.status(404).send("The provided book name was not found. Make sure the format is 3-letters capitalized: GEN, ROM, 1CO.")
+
+    const result = Bible[book][chapter];
+    if (!result || result === null) return res.status(404).send("Couldn't find the chapter specified. Please check the URL to make sure it's in the correct format.")
+
+    return res.status(200).json(result);
+})
+
+app.get("/:book/:chapter/:verse", (req, res) => {
+    const book = req.params.book;
+    let chapter = req.params.chapter;
+    const verse = req.params.verse;
+
+    if (!chapter || chapter < 1 || chapter > 150) chapter = 1;
+
+    const BibleBooks = Object.keys(Bible);
+    if (!BibleBooks.includes(book)) return res.status(404).send("The provided book name was not found. Make sure the format is 3-letters capitalized: GEN, ROM, 1CO.")
+
+    const result = Bible[book][chapter][verse];
+    if (!result || result === null) return res.status(404).send("Couldn't find the verse provided. Please check the URL to make sure it is in the correct format.")
+
+    return res.status(200).json(result);
+})
+
+app.listen(port)
